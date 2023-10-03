5a9cf8b4cd35f0cbd8fff2a74370e6a97a23ba10
mhaut <mhaut@users.noreply.github.com>
2023-10-03 11:37:16 +0200

    add code

--- mapreduce/flights-by-carrier/FlightsByCarrierMapper.java
+++ mapreduce/flights-by-carrier/FlightsByCarrierMapper.java
@@ -1,28 +1,32 @@
+// import java.io.IOException;
+// import au.com.bytecode.opencsv.CSVParser;
+// import org.apache.hadoop.io.*;
+// import org.apache.hadoop.mapreduce.Mapper;
+//
+// //Mapper<KeyIn, ValueIn, KeyOut, ValueOut>
+// public class FlightsByCarrierMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+// 	@Override
+// 	protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+// 		if (key.get() > 0) { //Ignora la primera linea
+// 			String[] lines = new CSVParser().parseLine(value.toString());
+// 			context.write(new Text(lines[8]), new IntWritable(1));
+// 		}
+// 	}
+// }
 import java.io.IOException;
-//import au.com.bytecode.opencsv.CSVParser;
 import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapreduce.Mapper;
 
-//Mapper<KeyIn, ValueIn, KeyOut, ValueOut>
 public class FlightsByCarrierMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
-	@Override
-	protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-		if (key.get() > 0) { //Ignora la primera linea
-//			String[] lines = new CSVParser().parseLine(value.toString());
-			List<String[]> lines = parseCSV(csvData);
-			context.write(new Text(lines[8]), new IntWritable(1));
-		}
-	}
-
-	public static List<String[]> parseCSV(String csvData) throws IOException {
-	List<String[]> records = new ArrayList<>();
-	try (BufferedReader reader = new BufferedReader(new StringReader(csvData))) {
-		String line;
-		while ((line = reader.readLine()) != null) {
-			String[] fields = line.split(",");
-			records.add(fields);
-		}
-	}
-	return records;
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        if (key.get() > 0) { // Ignora la primera línea
+            String[] fields = value.toString().split(",");
+            if (fields.length >= 9) {
+                Text carrier = new Text(fields[8]); // Tomar el campo en la posición 8 como clave
+                IntWritable one = new IntWritable(1);
+                context.write(carrier, one);
+            }
+        }
     }
 }
