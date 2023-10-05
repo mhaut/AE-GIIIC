f973473853c11475c45670b686d0dd199f476823
mhaut <mhaut@users.noreply.github.com>
2023-10-05 10:19:19 +0200

    add first version

--- mapreduce/flights-by-carrier/FlightsByCarrierMapper.java
+++ mapreduce/flights-by-carrier/FlightsByCarrierMapper.java
@@ -1,18 +1,3 @@
-// import java.io.IOException;
-// import au.com.bytecode.opencsv.CSVParser;
-// import org.apache.hadoop.io.*;
-// import org.apache.hadoop.mapreduce.Mapper;
-//
-// //Mapper<KeyIn, ValueIn, KeyOut, ValueOut>
-// public class FlightsByCarrierMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
-// 	@Override
-// 	protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-// 		if (key.get() > 0) { //Ignora la primera linea
-// 			String[] lines = new CSVParser().parseLine(value.toString());
-// 			context.write(new Text(lines[8]), new IntWritable(1));
-// 		}
-// 	}
-// }
 import java.io.IOException;
 import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapreduce.Mapper;
--- mapreduce/flights-by-carrier/build.sh
+++ mapreduce/flights-by-carrier/build.sh
@@ -2,7 +2,7 @@
 echo ">> Cleaning files..."
 rm -f *.class
 rm -f *.jar
-export CLASSPATH=/etc/hadoop/conf:/usr/lib/hadoop/lib/*:/usr/lib/hadoop/.//*:/usr/lib/hadoop-hdfs/./:/usr/lib/hadoop-hdfs/lib/*:/usr/lib/hadoop-hdfs/.//*:/usr/lib/hadoop-yarn/lib/*:/usr/lib/hadoop-yarn/.//*:/usr/lib/hadoop-mapreduce/lib/*:/usr/lib/hadoop-mapreduce/.//*:./lib/opencsv-2.3.jar:./build/*
+export CLASSPATH=/etc/hadoop/conf:/usr/lib/hadoop/lib/*:/usr/lib/hadoop/.//*:/usr/lib/hadoop-hdfs/./:/usr/lib/hadoop-hdfs/lib/*:/usr/lib/hadoop-hdfs/.//*:/usr/lib/hadoop-yarn/lib/*:/usr/lib/hadoop-yarn/.//*:/usr/lib/hadoop-mapreduce/lib/*:/usr/lib/hadoop-mapreduce/.//*
 echo ">> Compiling..."
 javac -classpath $CLASSPATH *.java
 jar cvf FlightsByCarrier.jar *.class
